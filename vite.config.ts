1507a9249872031113ef5c7e992d3b7304060f7b
mHwak22 <mohakyo123@gmail.com>
2024-04-12 10:48:28 +0530

    test2

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default {
+export default defineConfig({
+  plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-gesture-responder']
-    }
-  }
-}
-
+      external: ['react-gesture-responder'],
+    },
+  },
+});
