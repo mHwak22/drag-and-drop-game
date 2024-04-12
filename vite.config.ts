f3e9b74173ff40b5d0cdae6f3f4833c8130ce195
mHwak22 <mohakyo123@gmail.com>
2024-04-12 10:56:13 +0530

    test3

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path'; // Import the path module
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,4 +10,14 @@ export default defineConfig({
       external: ['react-gesture-responder'],
     },
   },
+  // Specify the entry point for your project
+  resolve: {
+    alias: {
+      // Adjust the path based on your project structure
+      // Assuming "src" directory contains your entry point JavaScript file
+      '@': path.resolve(__dirname, 'src') 
+    }
+  },
+  // Specify the entry point JavaScript file
+  root: './', // Adjust if your entry point file is named differently
 });
